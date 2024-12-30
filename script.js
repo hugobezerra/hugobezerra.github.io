4929e4bdd1a40a96b6f47f1b862321310bd8c20f
Hugo Bezerra <hugoinfor@gmail.com>
2024-12-30 10:58:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // Função para pesquisar tópicos e fórmulas
 function pesquisar() {
+    // Obtém o valor do campo de pesquisa, converte para minúsculas para facilitar a comparação
     const input = document.getElementById("campoPesquisa").value.toLowerCase();
+    
+    // Obtém todos os elementos com a classe "conteudo" e os armazena em uma coleção
     const elementos = document.getElementsByClassName("conteudo");
+    
+    // Itera sobre cada elemento da coleção "elementos"
     for (let elemento of elementos) {
+        // Verifica se o texto do elemento (também convertido para minúsculas) contém o valor do input
         if (elemento.innerText.toLowerCase().includes(input)) {
+            // Se o texto do elemento contém o valor do input, exibe o elemento
             elemento.style.display = "block";
         } else {
+            // Se o texto do elemento não contém o valor do input, oculta o elemento
             elemento.style.display = "none";
         }
     }
